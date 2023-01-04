31df2023ef6c9341475b2c5881f8e38c0ce100fc
Jumi Yoon <jums.code@gmail.com>
2023-01-03 19:34:30 -0800

    commit tree 8a10ea7084235cbe61340917447fe0acea590788

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,12 @@ function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+  function onItemFormSubmit(newItem) {
+    const newArray = [...items, newItem]
+    setItems(newArray);
+    console.log(newArray)
+  }
+
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
@@ -14,7 +20,7 @@ function App() {
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={onItemFormSubmit}/>
     </div>
   );
 }
